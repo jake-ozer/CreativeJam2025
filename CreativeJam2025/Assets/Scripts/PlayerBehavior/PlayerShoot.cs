f0b1b33efc2300daa3ac78d5b1d6a1d4ce0bacc3
Jake Ozer <jakobozer1@gmail.com>
2025-10-24 16:08:54 -0500

    mid rpog

---
+++ CreativeJam2025/Assets/Scripts/PlayerBehavior/PlayerShoot.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    //this script handles the shooting of fire and ice
+
+    [SerializeField] private GameObject fireProjectile;
+    [SerializeField] private GameObject iceProjectile;
+}
